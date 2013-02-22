79e81abcdb698dd285b85d8d2831e1ffdcd88dcb
Carl S. Yestrau Jr. <spam@featureblend.com>
2013-02-21 23:22:37 -0800

    Update backboneclass.js

--- backboneclass.js
+++ backboneclass.js
@@ -2,7 +2,7 @@ Backbone.Class = function() {
     this.cid = _.uniqueId('c');
     this.initialize.apply(this, arguments);
 };
-_.extend(RouteHandler.prototype, Events, {
+_.extend(Backbone.Class.prototype, Events, {
     initialize: function(){}
 });
 Backobone.Class.extend = Backbone.Model.extend;
