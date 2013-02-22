eb248831c86a9e8a2edf27be8bfa2e0e1262f61f
Carl S. Yestrau Jr. <spam@featureblend.com>
2013-02-21 23:42:22 -0800

    Update backclass.js

--- backclass.js
+++ backclass.js
@@ -2,7 +2,7 @@ Backbone.Class = function() {
     this.cid = _.uniqueId('c');
     this.initialize.apply(this, arguments);
 };
-_.extend(Backbone.Class.prototype, Events, {
+_.extend(Backbone.Class.prototype, Backbone.Events, {
     initialize: function(){}
 });
 Backobone.Class.extend = Backbone.Model.extend;
